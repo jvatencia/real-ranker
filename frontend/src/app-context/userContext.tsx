df662362f0f85bf81d4c04aac73ad90bc3e64203
Roman Kaufman <roman.a.kaufman@gmail.com>
2023-09-04 13:20:41 -0400

    context added

---
+++ frontend/src/app-context/userContext.tsx
@@ -0,0 +1,44 @@
+// FILE - ./src/app-context/user-context.tsx
+// ----------------------------------
+// stolen from: https://clerk.com/blog/understanding-and-properly-using-react-global-state?utm_source=www.google.com&utm_medium=referral&utm_campaign=none
+
+import React from "react";
+
+export interface UserContract {
+  id?: number,
+  username?: string,
+  firstName?: string,
+  email?: string
+}
+
+// The dummy user object used for this example
+export const DummyUser:UserContract ={
+  id: 1,
+  username: "MyUserName",
+  firstName: "Roman",
+  email: "john@doe.com"
+}
+
+/**
+ * Application state interface (ADD STUFF HERE FOR IT TO BE GLOBAL)
+ */
+export interface AppState {
+  user?: UserContract;
+  colleges?: Array<object>;
+  form?: object;
+  updateState: (newState: Partial<AppState>) => void;
+}
+
+/**
+ * Default application state
+ */
+const defaultState: AppState = {
+  user: {},
+  colleges: [],
+  updateState: (newState?: Partial<AppState>) => {},
+};
+
+/**
+ * Creating the Application state context for the provider
+ */
+export const UserContext = React.createContext<AppState>(defaultState);
