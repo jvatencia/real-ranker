f9596ab6423ff40e72b1aa74d8cf7372b2fcb917
Roman Kaufman <roman.a.kaufman@gmail.com>
2023-10-22 18:23:17 -0400

    added utilities

--- frontend/src/utils/utilities.tsx
+++ frontend/src/utils/utilities.tsx
@@ -2,40 +2,41 @@ function toPercent(val: number){
   return Math.round(val*100) + '%';
 }
 function toLetterGrade(val: number) {
-  if (val > .9666) {
+  console.log(val, typeof val);
+  if (val > .9333) {
     return 'A+';
   }
-  else if ( val > .9333) {
+  else if ( val > .8666) {
     return 'A';
   }
-  else if ( val > .90) {
+  else if ( val > .80) {
     return 'A-';
   }
-  else if ( val > .8666) {
+  else if ( val > .7333) {
     return 'B+';
   }
-  else if ( val > .8333) {
+  else if ( val > .6666) {
     return 'B';
   }
-  else if ( val > .80) {
+  else if ( val > .6) {
     return 'B-';
   }
-  else if ( val > .7666) {
+  else if ( val > .5333) {
     return 'C+';
   }
-  else if ( val > .7333) {
+  else if ( val > .4666) {
     return 'C';
   }
-  else if ( val > .70) {
+  else if ( val > .4) {
     return 'C-';
   }
-  else if ( val > .6666) {
+  else if ( val > .3333) {
     return 'D+';
   }
-  else if ( val > .6333) {
+  else if ( val > .2666) {
     return 'D';
   }
-  else if ( val > .60) {
+  else if ( val > .2) {
     return 'D-';
   }
   else {
