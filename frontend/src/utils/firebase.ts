ab36caf53094f6c9cd2c179a3211e222f90e1e40
jv atencia <jhayvee@thetestguy.com>
2024-03-23 00:25:49 +0800

    updated env keys

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -2,9 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 };
+
+console.log('[firebaseConfig]', firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
